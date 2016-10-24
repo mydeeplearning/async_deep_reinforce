d1867e9ee03f1d27a3cea7fea19da3ee02c4a62a
yinchuandong <johncdyin@gmail.com>
2016-10-24 23:31:41 +1100

    new: add flappy bird

--- a3c_display.py
+++ a3c_display.py
@@ -17,37 +17,38 @@ from constants import GRAD_NORM_CLIP
 from constants import USE_GPU
 from constants import USE_LSTM
 
+
 def choose_action(pi_values):
-  values = []
-  sum = 0.0
-  for rate in pi_values:
-    sum = sum + rate
-    value = sum
-    values.append(value)
-    
-  r = random.random() * sum
-  for i in range(len(values)):
-    if values[i] >= r:
-      return i;
-  #fail safe
-  return len(values)-1
+    values = []
+    sum = 0.0
+    for rate in pi_values:
+        sum = sum + rate
+        value = sum
+        values.append(value)
+
+    r = random.random() * sum
+    for i in range(len(values)):
+        if values[i] >= r:
+            return i
+    # fail safe
+    return len(values) - 1
 
 # use CPU for display tool
 device = "/cpu:0"
 
 if USE_LSTM:
-  global_network = GameACLSTMNetwork(ACTION_SIZE, -1, device)
+    global_network = GameACLSTMNetwork(ACTION_SIZE, -1, device)
 else:
-  global_network = GameACFFNetwork(ACTION_SIZE, device)
+    global_network = GameACFFNetwork(ACTION_SIZE, device)
 
 learning_rate_input = tf.placeholder("float")
 
-grad_applier = RMSPropApplier(learning_rate = learning_rate_input,
-                              decay = RMSP_ALPHA,
-                              momentum = 0.0,
-                              epsilon = RMSP_EPSILON,
-                              clip_norm = GRAD_NORM_CLIP,
-                              device = device)
+grad_applier = RMSPropApplier(learning_rate=learning_rate_input,
+                              decay=RMSP_ALPHA,
+                              momentum=0.0,
+                              epsilon=RMSP_EPSILON,
+                              clip_norm=GRAD_NORM_CLIP,
+                              device=device)
 
 # training_threads = []
 # for i in range(PARALLEL_SIZE):
@@ -65,18 +66,17 @@ sess.run(init)
 saver = tf.train.Saver()
 checkpoint = tf.train.get_checkpoint_state(CHECKPOINT_DIR)
 if checkpoint and checkpoint.model_checkpoint_path:
-  saver.restore(sess, checkpoint.model_checkpoint_path)
-  print("checkpoint loaded:", checkpoint.model_checkpoint_path)
+    saver.restore(sess, checkpoint.model_checkpoint_path)
+    print("checkpoint loaded:", checkpoint.model_checkpoint_path)
 else:
-  print("Could not find old checkpoint")
+    print("Could not find old checkpoint")
 
 game_state = GameState(0, display=True, no_op_max=0)
 
 while True:
-  pi_values = global_network.run_policy(sess, game_state.s_t)
+    pi_values = global_network.run_policy(sess, game_state.s_t)
 
-  action = choose_action(pi_values)
-  game_state.process(action)
+    action = choose_action(pi_values)
+    game_state.process(action)
 
-  game_state.update()
-
+    game_state.update()
